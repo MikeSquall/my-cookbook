d93c745a2066924924e10e18cb09e7e434e81739
mikearbib <arbib.michael@gmail.com>
2019-02-26 21:23:20 +0200

    Added Jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  transform: {
+    '\\.ts$': 'ts-jest',
+  },
+  testRegex: '.*\\.test\\.ts$',
+  moduleFileExtensions: ['ts', 'js'],
+  moduleDirectories: ['src', 'node_modules'],
+  testEnvironment: 'node',
+};
